0417ad5feaf0312adb4cf0274687e62f49f30b4a
Hamza Maach <hamzamaach56@gmail.com>
2025-04-19 12:46:49 +0100

    add ability's component

---
+++ frontend/components/ability.js
@@ -0,0 +1,28 @@
+import { defineComponent, h } from "../../../framework/src/index.js";
+import { TILE_SIZE } from "../constants/game-constants.js";
+
+export const AbilityComponent = defineComponent({
+
+    render() {
+        const size = TILE_SIZE * 0.65;
+        const offset = (TILE_SIZE - size) / 2;
+        const x = this.props.col * TILE_SIZE + offset;
+        const y = this.props.row * TILE_SIZE;
+
+        return h("div", {
+            style: {
+                position: "absolute",
+                transform: `translate(${x}px, ${y}px)`,
+            }
+        }, [
+            h("img", {
+                class: "ability " + this.props.type,
+                src: `./assets/abilities/${this.props.type}.png`,
+                style: {
+                    width: `${size}px`,
+                    height: `${size}px`,
+                }
+            })
+        ]);
+    }
+});
