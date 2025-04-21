e788a1b171c0bbdc0303e5cd969b46c28ce5e287
Hamza Maach <hamzamaach56@gmail.com>
2025-04-21 10:58:59 +0100

    add HUD component of the game

---
+++ frontend/components/hud.js
@@ -0,0 +1,15 @@
+import { defineComponent, h } from "https://unpkg.com/obsydianjs@latest";
+
+export const HudComponent = defineComponent({
+
+    render() {
+
+        return h("div", {
+            style: {
+                position: "absolute",
+            }
+        }, [
+
+        ]);
+    }
+});
